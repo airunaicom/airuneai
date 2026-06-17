f92089ceffbf79e2d98c3bea1b4396b56c63004e
airunaicom <ci@zboaragratis.ro>
2026-06-17 14:18:07 +0800

    Create callback.js

---
+++ functions/auth/callback.js
@@ -0,0 +1,37 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+
+  if (!code) {
+    return new Response("Missing code", { status: 400 });
+  }
+
+  // Schimbă codul cu un token de acces
+  const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      client_id: env.GH_ID,
+      client_secret: env.GH_SECRET,
+      code: code,
+    }),
+  });
+
+  const tokenData = await tokenResponse.json();
+
+  if (tokenData.error) {
+    return new Response(`Auth error: ${tokenData.error_description}`, { status: 400 });
+  }
+
+  const accessToken = tokenData.access_token;
+
+  // Redirecționează cu tokenul
+  return Response.redirect(
+    `https://airuneai.com/?token=${accessToken}`,
+    302
+  );
+}
